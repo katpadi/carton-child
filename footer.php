ecdba8d4bc907e93ffa361ec83cbf51f3980ae19
Kat Padilla <katpadillaaa@gmail.com>
2014-07-06 03:13:04 +0800

    credits

---
+++ footer.php
@@ -0,0 +1,24 @@
+  </div> <!-- #main.row -->
+
+</div> <!-- #page.grid -->
+
+<footer id="footer" role="contentinfo">
+
+  <div id="footer-content" class="grid wfull">
+
+    <div class="row">
+
+      <p class="copyright c12">
+        <span class="fl"><?php printf( __( 'Copyright &copy; %s %s. All Rights Reserved.', 'carton' ), date( 'Y' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
+        <span class="credit-link fr"><i class="icon-hand-right"></i>Big thanks to bavotasan.com as well! :)</span>
+      </p><!-- .c12 -->
+
+    </div><!-- .row -->
+
+  </div><!-- #footer-content.grid -->
+
+</footer><!-- #footer -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
